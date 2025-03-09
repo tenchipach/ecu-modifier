bd87d7eb7822c29f76b6be3b4327b776e4e33747
TuNombre <tuemail@example.com>
2025-03-09 03:02:37 +0100

    Moviendo archivos a la raíz del repositorio

--- ecu_file_modifier/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, UploadFile, File
-
-app = FastAPI()
-
-@app.post("/modify_binary_file/")
-async def modify_binary_file(file: UploadFile = File(...), modification_type: str = "invert"):
-    contents = await file.read()
-    return {"message": f"Archivo {file.filename} recibido con modificación {modification_type}"}
